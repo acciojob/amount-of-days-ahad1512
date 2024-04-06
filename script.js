bb0458b75c756407c360a2c5f93ff589fe008f88
ahad1512 <139266271+ahad1512@users.noreply.github.com>
2024-04-07 02:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-//your JS code here. If required.
-function daysOfAYear(year) {
-    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-        return 366; // Leap year
-    } else {
-        return 365; // Non-leap year
-    }
+function daysOfAYear(y) {
+	if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
+		return 366;
+	}
+	else{
+
+		return 365
+	}
 }
-alert(console.log(daysOfAYear(2024))); 
+let year=prompt("enter year");
+alert(daysOfAYear(year));
